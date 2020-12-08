2851345b2df1859f536f2f0a062768929dbe87de
Florian3011 <ries.flo@web.de>
2020-12-08 16:35:46 +0100

    db anbindung

--- HTML/main.php
+++ HTML/main.php
@@ -1,3 +1,22 @@
+<?php
+
+    $servername = "sql7.freesqldatabase.com";
+    $db_username = "sql7380944";
+    $password = "9GPYVgvBXt";
+    $db_name = "sql7380944";
+
+    // Verbindung erstellen:
+    $conn = new mysqli($servername, $db_username, $password, $db_name);
+
+    // Verbindung überprüfen: 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+?>
+
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -28,19 +47,19 @@
                         <input type="submit" value="Login" class="btn solid" />            
                     </form>
 
-                    <form action="#" class="sign-up-form">
+                    <form action="insert.php" method="POST" class="sign-up-form">
                         <h2 class="title">Sign up</h2>
                         <div class="input-field">
                             <i class="fas fa-user"></i>
-                            <input type="text" placeholder="Username" />
+                            <input type="text" placeholder="Username" name="username" required/>
                         </div>
                         <div class="input-field">
                             <i class="fas fa-envelope"></i>
-                            <input type="email" placeholder="Email" />
+                            <input type="email" placeholder="Email" name="email" required/>
                         </div>
                         <div class="input-field">
                             <i class="fas fa-lock"></i>
-                            <input type="password" placeholder="Password" />
+                            <input type="password" placeholder="Password" name="password" required/>
                         </div>
                         <input type="submit" class="btn" value="Sign up" />
                     </form>
