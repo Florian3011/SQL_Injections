3ebd4b5045d3a43dfb71550515453f7af5a12705
LisaEhmann <65292175+LisaEhmann@users.noreply.github.com>
2020-12-12 13:34:12 +0100

    profil page createt

---
+++ HTML/profile.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+            src="https://kit.fontawesome.com/5281e28dd9.js"
+            crossorigin="anonymous"
+        ></script>
+        <link rel="stylesheet" href="CSS\style.css" />
+        <title>profile Form</title>
+    </head>
+    <body>
+        <header>
+            <div id="head"> 
+                <h1 class="title">Profile</h1>
+            </div>
+            <div id="logout">
+                <h1 class="title">Log Out</h1>
+            </div>
+        </header>
+
+        <section id="profile">
+            <h1 class="profile">Profil von dir!</h1>
+            <hr>
+            <form>
+                <p>
+                        <label for="u_ID">Benutzer-ID: </label>
+                        <br>
+                        <label for="u_Name">Benutzername: </label>
+                        <br>
+                        <label for="u_PW">Passwort: </label>
+                        <br>
+                        <label for="u_Email">E-Mail Adresse: </label>
+                        <br>
+                        <label for="u_admin">Admin: </label>
+
+                </p>
+                
+
+            
+            </form>
+
+
+
+        </section>
+
+
+
+
+
+
+
+    </body>
+
+</html>
